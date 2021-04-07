549b9bcf394fc8fa3b1e94bb736f8b9b71d8374c
Sayed <sayed.aboelela96@gmail.com>
2021-04-07 17:21:11 +0200

    category_scroll_fail

--- src/components/header/CategoryHeader.tsx
+++ src/components/header/CategoryHeader.tsx
@@ -1,207 +1,226 @@
 import React, {FC} from 'react';
-import {Platform, StyleProp, StyleSheet, Text, TextStyle, TouchableOpacity, View, ViewStyle,} from 'react-native';
+import {
+  Animated,
+  Platform,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
 import {Colors, Fonts, Pixel, ScreenOptions,} from '../../constants/styleConstants';
 import {commonStyles} from '../../styles/styles';
 import {NavigationProps} from '../../constants/interfaces';
 import {
-    ArrowHeaderIcon,
-    CartIcon,
-    HeaderSearchIcon,
-    LogoIcon,
-    MenuIcon,
-    NotificationIcon,
-    SearchIcon,
+  ArrowHeaderIcon,
+  CartIcon,
+  HeaderSearchIcon,
+  LogoIcon,
+  MenuIcon,
+  NotificationIcon,
+  SearchIcon,
 } from '../../../assets/Icons/Icons';
 import IconTouchableContainer from '../touchables/IconTouchableContainer';
 import {useTranslation} from 'react-i18next';
 import Input from '../textInputs/Input';
 
 interface ICategoryHeader {
-    title: string;
-    containerStyle?: StyleProp<ViewStyle>;
-    titleStyle?: StyleProp<TextStyle>;
-    toggleHeader: Boolean;
-    handleToggleHeader: () => void;
+  title: string;
+  containerStyle?: StyleProp<ViewStyle>;
+  titleStyle?: StyleProp<TextStyle>;
+  toggleHeader: Boolean;
+  handleToggleHeader: () => void;
+  fadeAnim: Animated.Value;
 }
 
 const SearchSubmitBtn: FC = () => {
-    return (
-        <IconTouchableContainer style={styles.submitSearchBtn}>
-            <SearchIcon width={17} height={17}/>
-        </IconTouchableContainer>
-    );
+  return (
+    <IconTouchableContainer style={styles.submitSearchBtn}>
+      <SearchIcon width={17} height={17}/>
+    </IconTouchableContainer>
+  );
 };
 
 const CategoryHeader: FC<NavigationProps & ICategoryHeader> = ({
-                                                                   navigate,
-                                                                   goBack,
-                                                                   name,
-                                                                   title,
-                                                                   containerStyle,
-                                                                   titleStyle,
-                                                                   toggleHeader,
-                                                                   handleToggleHeader,
+                                                                 navigate,
+                                                                 goBack,
+                                                                 name,
+                                                                 title,
+                                                                 containerStyle,
+                                                                 titleStyle,
+                                                                 toggleHeader,
+                                                                 handleToggleHeader,
+                                                                 fadeAnim
                                                                }) => {
-    const {t} = useTranslation();
-
-    return (
-        <View style={[styles.mainContainer, containerStyle]}>
-            <View style={[styles.rowConatiner]}>
-                <View style={styles.right}>
-                    {!toggleHeader && (<View style={styles.titleConatiner}>
-                        <LogoIcon width={56} height={30}/>
-                    </View>)}
-                    <IconTouchableContainer onPress={global.DrawerProps.openDrawer}>
-                        <MenuIcon/>
-                    </IconTouchableContainer>
-                </View>
-
-                <View style={styles.centerContainer}>
-                    {!toggleHeader && (
-                        <>
-                            <Text style={styles.addressTitle}>{t('Deliver To')}</Text>
-                            <TouchableOpacity
-                                style={{
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                    justifyContent: 'space-between',
-                                }}>
-                                <Text style={styles.addressText}>Alexandria - Miami</Text>
-                                <ArrowHeaderIcon/>
-                            </TouchableOpacity>
-                        </>
-                    )}
-                    {toggleHeader && (
-                        <View style={[styles.titleConatiner, {marginLeft: 0}]}>
-                            <Text
-                                style={[styles.title, titleStyle]}
-                                ellipsizeMode="tail"
-                                numberOfLines={1}>
-                                {title ? title : ''}
-                            </Text>
-                        </View>
-                    )}
-                </View>
-
-                <View style={styles.leftContainer}>
-
-                    {!toggleHeader ? (
-                        <IconTouchableContainer onPress={() => navigate('Cart')}>
-                            <CartIcon/>
-                        </IconTouchableContainer>
-                    ) : (
-                        <IconTouchableContainer onPress={handleToggleHeader}>
-                            <HeaderSearchIcon/>
-                        </IconTouchableContainer>
-                    )}
-
-                    <IconTouchableContainer>
-                        <NotificationIcon/>
-                    </IconTouchableContainer>
-                </View>
-            </View>
-
-            {!toggleHeader && (<View style={styles.searchInputContainer}>
-                <Input
-                    options={{
-                        placeholder: t('What You Are Looking For ?'),
-                        placeholderTextColor: '#949494',
-                    }}
-                    contentContainerStyle={{borderRadius: 22, borderWidth: 0, padding: 0}}
-                    rightContent={() => <SearchSubmitBtn/>}
-                    iconRightStyle={{top: 5}}
-                />
-            </View>)}
-
+  const {t} = useTranslation();
+  console.log('fadeAnim', fadeAnim)
+  return (
+    <View style={[styles.mainContainer, containerStyle]}>
+      <View style={[styles.rowConatiner]}>
+        <View style={styles.right}>
+          <Animated.View style={[styles.titleConatiner, {
+            opacity: fadeAnim,
+            visibility: hidden,
+            transform: [{
+              translateY: fadeAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [-150, 0]  // 0 : 150, 0.5 : 75, 1 : 0
+              }),
+            }]
+          }]}>
+            <LogoIcon width={56} height={30}/>
+          </Animated.View>
+          <IconTouchableContainer onPress={global.DrawerProps.openDrawer}>
+            <MenuIcon/>
+          </IconTouchableContainer>
         </View>
-    );
+
+        <View style={styles.centerContainer}>
+            <Animated.View style={{opacity: fadeAnim}}>
+              <Text style={styles.addressTitle}>{t('Deliver To')}</Text>
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}>
+                <Text style={styles.addressText}>Alexandria - Miami</Text>
+                <ArrowHeaderIcon/>
+              </TouchableOpacity>
+            </Animated.View>
+          {toggleHeader && (
+            <View style={[styles.titleConatiner, {marginLeft: 0}]}>
+              <Text
+                style={[styles.title, titleStyle]}
+                ellipsizeMode="tail"
+                numberOfLines={1}>
+                {title ? title : ''}
+              </Text>
+            </View>
+          )}
+        </View>
+
+        <View style={styles.leftContainer}>
+
+          {!toggleHeader ? (
+            <IconTouchableContainer onPress={() => navigate('Cart')}>
+              <CartIcon/>
+            </IconTouchableContainer>
+          ) : (
+            <IconTouchableContainer onPress={handleToggleHeader}>
+              <HeaderSearchIcon/>
+            </IconTouchableContainer>
+          )}
+
+          <IconTouchableContainer>
+            <NotificationIcon/>
+          </IconTouchableContainer>
+        </View>
+      </View>
+
+      {!toggleHeader && (<View style={styles.searchInputContainer}>
+        <Input
+          options={{
+            placeholder: t('What You Are Looking For ?'),
+            placeholderTextColor: '#949494',
+          }}
+          contentContainerStyle={{borderRadius: 22, borderWidth: 0, padding: 0}}
+          rightContent={() => <SearchSubmitBtn/>}
+          iconRightStyle={{top: 5}}
+        />
+      </View>)}
+
+    </View>
+  );
 }
 
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        backgroundColor: Colors.sacandAppBackgroundColor,
-        paddingTop: ScreenOptions.StatusBarHeight + 15,
-        minHeight:
-            Platform.OS === 'android'
-                ? 56 + ScreenOptions.StatusBarHeight
-                : 64 + ScreenOptions.StatusBarHeight,
-        paddingHorizontal: 15,
-        zIndex: 200,
-        // paddingBottom: Pixel(30),
+  mainContainer: {
+    backgroundColor: Colors.sacandAppBackgroundColor,
+    paddingTop: ScreenOptions.StatusBarHeight + 15,
+    minHeight:
+      Platform.OS === 'android'
+        ? 56 + ScreenOptions.StatusBarHeight
+        : 64 + ScreenOptions.StatusBarHeight,
+    paddingHorizontal: 15,
+    zIndex: 200,
+    // paddingBottom: Pixel(30),
+  },
+  rowConatiner: {
+    ...commonStyles.rowBox,
+    justifyContent: 'space-between',
+  },
+  right: {
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  leftContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleConatiner: {
+    ...commonStyles.rowBox,
+    marginLeft: 5,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginBottom: Pixel(10),
+  },
+  title: {
+    color: Colors.colorSacand,
+    fontSize: 20,
+    fontFamily: Fonts.bold,
+  },
+  cartContainer: {
+    backgroundColor: Colors.colorSacand,
+    width: 20,
+    height: 16,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderRadius: 6,
+    position: 'absolute',
+    zIndex: 3,
+    right: -7,
+    top: -4,
+  },
+  centerContainer: {
+    // marginRight: 'auto',
+    // flexDirection: 'row',
+    // alignItems: 'center',
+  },
+  addressTitle: {
+    textAlign: 'center',
+    color: Colors.dark,
+    fontFamily: Fonts.regular,
+    fontSize: Pixel(25),
+  },
+  addressText: {
+    textAlign: 'center',
+    color: Colors.colorSacand,
+    fontFamily: Fonts.medium,
+    fontSize: Pixel(25),
+    marginRight: Pixel(15),
+  },
+  searchInputContainer: {
+    marginTop: Pixel(50),
+    paddingBottom: Pixel(20),
+  },
+  submitSearchBtn: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-    rowConatiner: {
-        ...commonStyles.rowBox,
-        justifyContent: 'space-between',
-    },
-    right: {
-        flexDirection: 'row-reverse',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    leftContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    titleConatiner: {
-        ...commonStyles.rowBox,
-        marginLeft: 5,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        marginBottom: Pixel(10),
-    },
-    title: {
-        color: Colors.colorSacand,
-        fontSize: 20,
-        fontFamily: Fonts.bold,
-    },
-    cartContainer: {
-        backgroundColor: Colors.colorSacand,
-        width: 20,
-        height: 16,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        borderRadius: 6,
-        position: 'absolute',
-        zIndex: 3,
-        right: -7,
-        top: -4,
-    },
-    centerContainer: {
-        // marginRight: 'auto',
-        // flexDirection: 'row',
-        // alignItems: 'center',
-    },
-    addressTitle: {
-        textAlign: 'center',
-        color: Colors.dark,
-        fontFamily: Fonts.regular,
-        fontSize: Pixel(25),
-    },
-    addressText: {
-        textAlign: 'center',
-        color: Colors.colorSacand,
-        fontFamily: Fonts.medium,
-        fontSize: Pixel(25),
-        marginRight: Pixel(15),
-    },
-    searchInputContainer: {
-        marginTop: Pixel(50),
-        paddingBottom: Pixel(20),
-    },
-    submitSearchBtn: {
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.22,
-        shadowRadius: 2.22,
-        elevation: 3,
-        backgroundColor: Colors.sacandAppBackgroundColor,
-        padding: 4,
-        borderRadius: 15,
-    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    backgroundColor: Colors.sacandAppBackgroundColor,
+    padding: 4,
+    borderRadius: 15,
+  },
 });
 
 export default CategoryHeader;
--- src/screens/Category.tsx
+++ src/screens/Category.tsx
@@ -1,5 +1,5 @@
-import React, {FC, useEffect, useState} from 'react';
-import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {FC, useCallback, useEffect, useRef, useState} from 'react';
+import {Animated, FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {Container, Content} from '../components/containers/Containers';
 import {Colors, Fonts, Images, Pixel} from '../constants/styleConstants';
 import {useTranslation} from 'react-i18next';
@@ -11,242 +11,266 @@ import {useNavigation} from "@react-navigation/native";
 
 
 const Item = ({item, selectedCategory, handleSelectedCategory}) => (
-    <TouchableOpacity onPress={() => handleSelectedCategory(item.title)} style={[styles.headerCategoryListItem]}>
-        <View
-            style={[
-                styles.imageContainer,
-            ]}>
-            <FastImage
-                source={Images.offerSlider}
-                style={commonStyles.image}
-                resizeMode="cover"
-            />
-        </View>
-        <Text
-            style={[styles.categoryTitle, {color: selectedCategory === item.title ? Colors.colorSacand : Colors.dark}]}>{item.title}</Text>
-    </TouchableOpacity>
+  <TouchableOpacity onPress={() => handleSelectedCategory(item.title)} style={[styles.headerCategoryListItem]}>
+    <View
+      style={[
+        styles.imageContainer,
+      ]}>
+      <FastImage
+        source={Images.offerSlider}
+        style={commonStyles.image}
+        resizeMode="cover"
+      />
+    </View>
+    <Text
+      style={[styles.categoryTitle, {color: selectedCategory === item.title ? Colors.colorSacand : Colors.dark}]}>{item.title}</Text>
+  </TouchableOpacity>
 );
 
 
 const Category: FC = () => {
-    const {t} = useTranslation();
+  const {t} = useTranslation();
 
-    const DATA = [
-        {
-            id: 1,
-            title: t('Supermarket'),
-            image: 'Voucher 12457',
-        },
+  const DATA = [
+    {
+      id: 1,
+      title: t('Supermarket'),
+      image: 'Voucher 12457',
+    },
 
-        {
-            id: 2,
-            title: t('Beef'),
-            image: 'Voucher 12457',
-        },
+    {
+      id: 2,
+      title: t('Beef'),
+      image: 'Voucher 12457',
+    },
 
-        {
-            id: 3,
-            title: t('Chicken'),
-            image: 'Voucher 12457',
-        },
+    {
+      id: 3,
+      title: t('Chicken'),
+      image: 'Voucher 12457',
+    },
 
-        {
-            id: 4,
-            title: t('Fish'),
-            image: 'Voucher 12457',
-        },
+    {
+      id: 4,
+      title: t('Fish'),
+      image: 'Voucher 12457',
+    },
 
-        {
-            id: 5,
-            title: t('Fruit'),
-            image: 'Voucher 12457',
-        },
+    {
+      id: 5,
+      title: t('Fruit'),
+      image: 'Voucher 12457',
+    },
 
-        {
-            id: 6,
-            title: t('Vegetables'),
-            image: 'Voucher 12457',
-        },
+    {
+      id: 6,
+      title: t('Vegetables'),
+      image: 'Voucher 12457',
+    },
 
-        {
-            id: 7,
-            title: t('Vegetables'),
-            image: 'Voucher 12457',
-        },
-    ];
-    const STORES_DATA = [
-        {
-            id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-            title: t('Refresh Market'),
-            image: 'Text 1',
-            category: t('Supermarket')
-        },
-        {
-            id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-            title: t('Smile Market'),
-            image: 'Text 1',
-            category: t('Supermarket')
-        },
-        {
-            id: "3ac682afc-c605-48d3-a4f8-fbd91aa97f63",
-            title: t('Smile Market'),
-            image: 'Text 1',
-            category: t('Beef')
-        },
-        {
-            id: "3ac682afc-c605-48dw3-a4f8-fbd91aa97f63",
-            title: t('Smile Market'),
-            image: 'Text 1',
-            category: t('Chicken')
-        },
-        {
-            id: "3ac68afc-c60s5-48d3-a4f8-fbd91aa97f63",
-            title: t('Smile Market'),
-            image: 'Text 1',
-            category: t('Supermarket')
-        },
-        {
-            id: "3ac68afc-c605-48df3-a4f8-fbd91aa97f63",
-            title: t('Smile Market'),
-            image: 'Text 1',
-            category: t('Supermarket')
-        },
-        {
-            id: "3ac68afc-c60asd5-48d3-a4f8-fbd91aa97f63",
-            title: t('Smile Market'),
-            image: 'Text 1',
-            category: t('Supermarket')
-        },
-        {
-            id: "3ac68dsaafc-c605-48d3-a4f8-fbd91aa97f63",
-            title: t('Smile Market'),
-            image: 'Text 1',
-            category: t('Supermarket')
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: t('Restaurant Market'),
-            image: 'Text 1',
-            category: t('Fruit')
-        },
-        {
-            id: "58694a0f-3da1-471f-bsd96-145571e29d72",
-            title: t('Refresh Market'),
-            image: 'Text 1',
-            category: t('Fish')
-        },
-        {
-            id: "58694a0f-3da1-471f-bsdad96-145571e29d72",
-            title: t('Restaurant Market'),
-            image: 'Text 1',
-            category: t('Vegetables')
-        },
-    ];
+    {
+      id: 7,
+      title: t('Vegetables'),
+      image: 'Voucher 12457',
+    },
+  ];
+  const STORES_DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: t('Refresh Market'),
+      image: 'Text 1',
+      category: t('Supermarket')
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: t('Smile Market'),
+      image: 'Text 1',
+      category: t('Supermarket')
+    },
+    {
+      id: "3ac682afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: t('Smile Market'),
+      image: 'Text 1',
+      category: t('Beef')
+    },
+    {
+      id: "3ac682afc-c605-48dw3-a4f8-fbd91aa97f63",
+      title: t('Smile Market'),
+      image: 'Text 1',
+      category: t('Chicken')
+    },
+    {
+      id: "3ac68afc-c60s5-48d3-a4f8-fbd91aa97f63",
+      title: t('Smile Market'),
+      image: 'Text 1',
+      category: t('Supermarket')
+    },
+    {
+      id: "3ac68afc-c605-48df3-a4f8-fbd91aa97f63",
+      title: t('Smile Market'),
+      image: 'Text 1',
+      category: t('Supermarket')
+    },
+    {
+      id: "3ac68afc-c60asd5-48d3-a4f8-fbd91aa97f63",
+      title: t('Smile Market'),
+      image: 'Text 1',
+      category: t('Supermarket')
+    },
+    {
+      id: "3ac68dsaafc-c605-48d3-a4f8-fbd91aa97f63",
+      title: t('Smile Market'),
+      image: 'Text 1',
+      category: t('Supermarket')
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: t('Restaurant Market'),
+      image: 'Text 1',
+      category: t('Fruit')
+    },
+    {
+      id: "58694a0f-3da1-471f-bsd96-145571e29d72",
+      title: t('Refresh Market'),
+      image: 'Text 1',
+      category: t('Fish')
+    },
+    {
+      id: "58694a0f-3da1-471f-bsdad96-145571e29d72",
+      title: t('Restaurant Market'),
+      image: 'Text 1',
+      category: t('Vegetables')
+    },
+  ];
 
-    const {navigate} = useNavigation();
-    // const [contentOffsetY, setContentOffsetY] = useState(0);
-    const [toggleHeader, setToggleHeader] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [filteredData, setFilteredData] = useState([]);
+  const {navigate} = useNavigation();
+  // const [contentOffsetY, setContentOffsetY] = useState(0);
+  const [toggleHeader, setToggleHeader] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const slideInOut = useRef(new Animated.Value(1)).current;
+  const [active, setActive] = useState(true);
 
-    const handleOnScroll = (event: Object) => {
-        if (event.nativeEvent.contentOffset.y >= 40) {
-            setToggleHeader(true);
-        } else {
-            setToggleHeader(false);
-        }
+  const toggleActive = useCallback(() => {
+    setActive((e) => !e);
+    // onValueChange && onValueChange(active);
+    Animated.timing(slideInOut, {
+      toValue: active ? 0 : 1,
+      useNativeDriver: true,
+    }).start();
+  }, [active]);
+
+  const translate = {
+    transform: [
+      {
+        translateY: slideInOut.interpolate({
+          inputRange: [0, 1],
+          outputRange: [50, 0],
+        }),
+      },
+    ],
+  };
+
+  const handleOnScroll = (event: Object) => {
+    if (event.nativeEvent.contentOffset.y >= 40) {
+      setToggleHeader(true);
+      toggleActive();
+    } else {
+      setToggleHeader(false);
+      toggleActive();
     }
+  }
 
-    const handleToggleHeader = () => {
-        setToggleHeader(!toggleHeader);
-    }
+  const handleToggleHeader = () => {
+    setToggleHeader(!toggleHeader);
+  }
 
-    const handleSelectedCategory = (category: string) => {
-        setSelectedCategory(category);
-        let data = STORES_DATA.filter(store => store.category === category);
-        setFilteredData(data);
-    }
+  const handleSelectedCategory = (category: string) => {
+    setSelectedCategory(category);
+    let data = STORES_DATA.filter(store => store.category === category);
+    setFilteredData(data);
+  }
 
-    useEffect(() => {
-        setSelectedCategory(t('Supermarket'));
-        let data = STORES_DATA.filter(store => store.category === t('Supermarket'));
-        setFilteredData(data);
-    }, []);
+  useEffect(() => {
+    setSelectedCategory(t('Supermarket'));
+    let data = STORES_DATA.filter(store => store.category === t('Supermarket'));
+    setFilteredData(data);
+  }, []);
 
-    return (
-        <Container style={styles.container}>
-            <CategoryHeader
-                navigate={navigate}
-                handleToggleHeader={handleToggleHeader} toggleHeader={toggleHeader}
-                title={selectedCategory}/>
-            <View style={styles.headerCategoryList}>
-                <FlatList
-                    data={DATA}
-                    renderItem={({item, index}) => <Item
-                        selectedCategory={selectedCategory}
-                        handleSelectedCategory={(title) => handleSelectedCategory(title)} item={item}/>}
-                    keyExtractor={(item) => item.id}
-                    horizontal
-                    showsVerticalScrollIndicator={false}
-                    showsHorizontalScrollIndicator={false}
-                />
-            </View>
-            <Content noPadding
-                     style={styles.contentContainer}
-                     options={{
-                         onScroll: handleOnScroll,
-                     }}>
-                <Text style={styles.sectionTitle}>{selectedCategory}</Text>
-                <CategoryStoresList data={filteredData}/>
-            </Content>
-        </Container>
-    );
+  return (
+    <Container style={styles.container}>
+      <CategoryHeader
+        navigate={navigate}
+        handleToggleHeader={handleToggleHeader} fadeAnim={translate} toggleHeader={toggleHeader}
+        title={selectedCategory}/>
+      <View style={styles.headerCategoryList}>
+        <FlatList
+          data={DATA}
+          renderItem={({item, index}) => <Item
+            selectedCategory={selectedCategory}
+            handleSelectedCategory={(title) => handleSelectedCategory(title)} item={item}/>}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+      <Content noPadding
+               style={styles.contentContainer}
+               options={{
+                 onScroll: handleOnScroll,
+               }}>
+        <Text style={styles.sectionTitle}>{selectedCategory}</Text>
+        <CategoryStoresList data={filteredData}/>
+      </Content>
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: Colors.sacandAppBackgroundColor,
-    },
-    contentContainer: {
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-    },
-    headerCategoryList: {
-        // paddingHorizontal: 20,
-        marginHorizontal: 20,
-        borderBottomWidth: 1,
-        borderColor: '#707070',
-        paddingTop: Pixel(10),
-        paddingBottom: Pixel(20),
-    },
-    headerCategoryListItem: {
-        // paddingHorizontal: 20,
-        // marginTop: Pixel(15),
-        marginRight: Pixel(20),
-        alignItems: 'center'
-    },
-    imageContainer: {
-        borderRadius: 8,
-        overflow: 'hidden',
-        ...commonStyles.boxShadow,
-        height: Pixel(75),
-        width: Pixel(140),
-        marginBottom: Pixel(5),
-    },
-    categoryTitle: {
-        fontFamily: Fonts.black,
-        fontSize: Pixel(25),
-        color: Colors.dark,
-        textAlign: 'center',
-        marginTop: Pixel(10),
-        textTransform: 'uppercase',
-        width: '100%',
-    },
-    sectionTitle: {
-        fontFamily: Fonts.black,
-        fontSize: Pixel(47),
-        color: Colors.colorSacand,
-    },
+  container: {
+    backgroundColor: Colors.sacandAppBackgroundColor,
+  },
+  contentContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  headerCategoryList: {
+    // paddingHorizontal: 20,
+    marginHorizontal: 20,
+    borderBottomWidth: 1,
+    borderColor: '#707070',
+    paddingTop: Pixel(10),
+    paddingBottom: Pixel(20),
+  },
+  headerCategoryListItem: {
+    // paddingHorizontal: 20,
+    // marginTop: Pixel(15),
+    marginRight: Pixel(20),
+    alignItems: 'center'
+  },
+  imageContainer: {
+    borderRadius: 8,
+    overflow: 'hidden',
+    ...commonStyles.boxShadow,
+    height: Pixel(75),
+    width: Pixel(140),
+    marginBottom: Pixel(5),
+  },
+  categoryTitle: {
+    fontFamily: Fonts.black,
+    fontSize: Pixel(25),
+    color: Colors.dark,
+    textAlign: 'center',
+    marginTop: Pixel(10),
+    textTransform: 'uppercase',
+    width: '100%',
+  },
+  sectionTitle: {
+    fontFamily: Fonts.black,
+    fontSize: Pixel(47),
+    color: Colors.colorSacand,
+  },
 });
 
 export default Category;
