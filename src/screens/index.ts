9784085dc134a282dbb718d1d6ed058f3075a5b3
eslammohamed14 <eslam.mohamed1412@gmail.com>
2021-04-17 13:43:47 +0200

    callNow ui

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -48,3 +48,4 @@ export const OrderOut = register({loader: () => import('./orderOut')});
 export const MyCards = register({loader: () => import('./MyCards')});
 export const TrackingOrder = register({loader: () => import('./TrackingOrder')});
 export const ProductPage = register({loader: () => import('./ProductPage')});
+export const CallNow = register({loader: () => import('./CallNow')});
